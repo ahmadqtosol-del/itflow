f81c582b564f9e112b401ad154e19e77d41fe132
Ahmad Qtos <ahmadqtosol@gmail.com>
2026-08-27 21:27:17 +0500

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,13 @@ app = FastAPI(title=settings.app_name)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_origin, "http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174"],
+    allow_origins=[
+    "https://itflow-9p6.pages.dev",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
+]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
